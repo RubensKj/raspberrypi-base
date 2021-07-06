4505fa84234674cc55d1801d6622109c4243dc1c
rubenskj <rubenskleinschmidtjr@gmail.com>
2021-07-06 19:17:34 -0300

    Fixing scripts

--- docker/portainer/install-service.sh
+++ docker/portainer/install-service.sh
@@ -1,8 +1,11 @@
 DIR="/etc/systemd/system/"
 
-if [ -d "$DIR" ] then
+if [ -d "$DIR" ]; then
   echo "Installing the portainer service"
   sudo cp ./portainer.service $DIR.
+  sudo systemctl start portainer.service
+  sudo systemctl enable portainer.service
+  sudo systemctl status portainer.service
 else
   echo "Wasn't able to install the portainer service..."
   echo "The '$DIR' doesn't exist."
